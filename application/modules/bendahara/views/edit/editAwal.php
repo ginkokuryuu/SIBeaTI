9b2863d147061bc1eb99423b2c6cc151196fb300
Habib Yahya <nmh.habib.y@gmail.com>
2020-07-19 08:55:13 +0700

    Upload File
    upload file dan di simpan di /public/uploads/

---
+++ application/modules/bendahara/views/edit/editAwal.php
@@ -0,0 +1,5 @@
+<div style="margin-left:5%; margin-right:5%;">
+    <div class="alert alert-primary" role="alert">
+        Ingat, ini tidak akan disimpan ke database sebelum di simpan!
+    </div>
+</div>
