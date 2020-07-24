6855714528908da98a177f1b12fcd742a907739c
Habib Yahya <nmh.habib.y@gmail.com>
2020-07-24 23:46:46 +0700

    perbaikan nama class

--- application/modules/auth/controllers/register.php
+++ application/modules/auth/controllers/register.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class register extends CI_Controller {
+class Register extends CI_Controller {
 
 	public function index()
 	{
