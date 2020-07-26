a3346fa20554cbb8cd2f4bd78355332ee45721a8
yukiyr <37041284+yukiyr@users.noreply.github.com>
2020-07-26 20:59:24 +0700

    new

---
+++ application/modules/dashboard/views/templateselektor.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $title ?></title>
+
+    <!-- Bootstrap core CSS-->
+    <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+
+    <!-- Custom fonts for this template-->
+    <link href="<?php echo base_url('assets/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
+
+    <!-- Page level plugin CSS-->
+    <link href="<?php echo base_url('assets/datatables/dataTables.bootstrap4.css') ?>" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="<?php echo base_url('css/sb-admin.css') ?>" rel="stylesheet">
+</head>
+
+<body id="page-top">
+
+	<?php $this->load->view("_partials/navbar.php") ?>
+	<div id="wrapper">
+
+		<?php $this->load->view("_partials/sidebarselektor.php") ?>
+
+		<div id="content-wrapper">
+
+            <?php echo $contents ?>
+
+			<!-- Sticky Footer -->
+			<?php $this->load->view("_partials/footer.php") ?>
+
+		</div>
+		<!-- /.content-wrapper -->
+
+	</div>
+	<!-- /#wrapper -->
+
+
+	<?php $this->load->view("_partials/scrolltop.php") ?>
+	<?php $this->load->view("_partials/modal.php") ?>
+
+	<?php $this->load->view("_partials/js.php") ?>
+
+	<script>
+	function deleteConfirm(url){
+		$('#btn-delete').attr('href', url);
+		$('#deleteModal').modal();
+	}
+	</script>
+</body>
+
+</html>
