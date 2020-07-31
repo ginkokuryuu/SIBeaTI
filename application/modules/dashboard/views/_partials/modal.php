98ae320595cff6b6279f0460f19f821d57129eba
izzahd <izhdinillah@gmail.com>
2020-07-31 22:47:22 +0700

    edit dashboard module (for voter, selektor)

--- application/modules/dashboard/views/_partials/modal.php
+++ application/modules/dashboard/views/_partials/modal.php
@@ -18,7 +18,7 @@
 </div>
 
 
-<!-- Logout Delete Confirmation-->
+<!-- Delete Confirmation-->
 <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -36,3 +36,80 @@
     </div>
   </div>
 </div>
+
+<!-- Submit Daftar Modal-->
+<div class="modal fade" id="submitDaftarModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Konfirmasi Submit</h5>
+        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+      </div>
+      <div class="modal-body">Review kembali biodata Anda sebelum mendaftar beasiswa ini. Submit untuk beasiswa ini?</div>
+      <div class="modal-footer">
+        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+        <a class="btn btn-primary" href="#" id="submitFormDaftar">Submit</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Submit Vote Modal-->
+<div class="modal fade" id="submitVoteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Konfirmasi Submit</h5>
+        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+      </div>
+      <div class="modal-body">Data yang disubmit tidak akan bisa diubah. Submit data ini?</div>
+      <div class="modal-footer">
+        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+        <a class="btn btn-primary" href="#" id="submitFormVote">Submit</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Submit Penerima Modal-->
+<div class="modal fade" id="submitPenerimaModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Konfirmasi Submit</h5>
+        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+      </div>
+      <div class="modal-body">Data yang disubmit tidak akan bisa diubah. Submit data ini?</div>
+      <div class="modal-footer">
+        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+        <a class="btn btn-primary" href="#" id="submitFormPenerima">Submit</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script src="<?php echo base_url('public/js/jquery.min.js') ?>"></script>
+<script>
+    $('#submitFormVote').click(function(){
+        /* when the submit button in the modal is clicked, submit the form */
+        $('#formVote').submit();
+    });
+</script>
+<script>
+    $('#submitFormPenerima').click(function(){
+        /* when the submit button in the modal is clicked, submit the form */
+        $('#formPenerima').submit();
+    });
+</script>
+<script>
+    $('#submitFormDaftar').click(function(){
+        /* when the submit button in the modal is clicked, submit the form */
+        $('#formDaftar').submit();
+    });
+</script>
