c50e20e02062a4b4b91a1f268c0dd4ad7a93f01a
Habib Yahya <nmh.habib.y@gmail.com>
2020-08-09 14:25:30 +0700

    Update Calon_model.php

--- application/modules/dashboard/models/Calon_model.php
+++ application/modules/dashboard/models/Calon_model.php
@@ -47,7 +47,7 @@ class Calon_model extends CI_Model
         $this->db->from('calon');
         $this->db->join('pendaftar', 'pendaftar.pendaftar_id = calon.pendaftar_id');
         $this->db->join('biodata', 'biodata.biodata_id = pendaftar.biodata_id');
-        $this->db->group_by('pendaftar.pendaftar_id');
+		$this->db->join('beasiswa', 'beasiswa.beasiswa_id = pendaftar.beasiswa_id');
         $this->db->where(['beasiswa.status_pemilihan'=>'Dibuka']);
         $this->db->group_by('pendaftar.pendaftar_id');
         $query = $this->db->get();
