9523036333bb4889fab2b45f8275cfc4474e0718
Habib Yahya <nmh.habib.y@gmail.com>
2020-08-11 15:24:26 +0700

    update format angka di rekap

--- application/modules/bendahara/views/rekap.php
+++ application/modules/bendahara/views/rekap.php
@@ -10,8 +10,59 @@ function changeNegNumber($number){
 }
 
 function saldoBerjalan($transaksi){
-    $result = $transaksi->nominal_donasi + $transaksi->nominal_penyaluran + $transaksi->lain_lain;
+    if($transaksi != ''){
+        $result = $transaksi->nominal_donasi + $transaksi->nominal_penyaluran + $transaksi->lain_lain;
+    }
+    else{
+        $result = '';
+    }
     return $result;
+}
+
+function formatAngka($angka){
+    if($angka != ''){
+        $len = strlen($angka);
+
+        if(substr($angka, 0, 1) == "-"){
+            $angka = substr($angka, 1, ($len - 1));
+            $len = strlen($angka);
+            $neg = "1";
+        }
+        else{
+            $neg = "0";
+        }
+
+
+        if($len % 3 == 0){
+            $count = floor($len / 3) - 1;
+        }
+        else{
+            $count = floor($len / 3);
+        }
+
+        $string = "";
+
+        for($i = 1; $i <= $count; $i++){
+            $string = "." . substr($angka, ($len - ($i * 3)), 3) . $string;
+        }
+
+        if($count != 0){
+            $string = substr($angka, 0, $count * (-3)) . $string;
+        }
+        else{
+            $string = substr($angka, 0, $len) . $string;
+        }
+
+        if($neg == "1"){
+            return "(" . $string . ")";
+        }
+        else{
+            return $string;
+        }
+    }
+    else{
+        return '';
+    }
 }
 ?>
 
@@ -48,20 +99,20 @@ function saldoBerjalan($transaksi){
             $count = 0;
             foreach($periodikPertahun as $perTah): ?>
             <tr class="text-right">
-                <td class="text-center" style="border-style: solid; background: navy; color: white;" scope="col"><?php echo $perTah->periode_tahun; ?></td>
-                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo $perTah->jumlah_donasi; ?></td>
-                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo $perTah->nominal_donasi; ?></td>
-                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo $perTah->jumlah_penerima; ?></td>
-                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo changeNegNumber($perTah->nominal_penyaluran); ?></td>
-                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo changeNegNumber($perTah->lain_lain); ?></td>
+                <td class="text-center" style="border-style: solid; background: navy; color: white;" scope="col"><?php echo $perTah->periode_tahun ?? '' ?></td>
+                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo $perTah->jumlah_donasi ?? '' ?></td>
+                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo formatAngka($perTah->nominal_donasi ?? '') ?></td>
+                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo $perTah->jumlah_penerima ?? '' ?></td>
+                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo formatAngka($perTah->nominal_penyaluran ?? '' )?></td>
+                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo formatAngka($perTah->lain_lain ?? '' )?></td>
                 
-                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo $kumulatifPertahun[$count]->jumlah_donasi; ?></td>
-                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo $kumulatifPertahun[$count]->nominal_donasi; ?></td>
-                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo $kumulatifPertahun[$count]->jumlah_penerima; ?></td>
-                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo changeNegNumber($kumulatifPertahun[$count]->nominal_penyaluran); ?></td>
-                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo changeNegNumber($kumulatifPertahun[$count]->lain_lain); ?></td>
+                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo $kumulatifPertahun[$count]->jumlah_donasi ?? '' ?></td>
+                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo formatAngka($kumulatifPertahun[$count]->nominal_donasi ?? '') ?></td>
+                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo $kumulatifPertahun[$count]->jumlah_penerima ?? '' ?></td>
+                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo formatAngka($kumulatifPertahun[$count]->nominal_penyaluran  ?? '' )?></td>
+                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo formatAngka($kumulatifPertahun[$count]->lain_lain  ?? '' )?></td>
 
-                <td style="border-style: solid; background: red; color: white;" scope="col"><?php echo saldoBerjalan($kumulatifPertahun[$count]); ?></td>
+                <td style="border-style: solid; background: red; color: white;" scope="col"><?php echo formatAngka(saldoBerjalan($kumulatifPertahun[$count] ?? '' )) ?></td>
                 <td class="text-left" style="border-style: solid;" scope="col">-</td>
             </tr>
             <?php $lastKumulatif = $kumulatifPertahun[$count];
@@ -69,41 +120,41 @@ function saldoBerjalan($transaksi){
             endforeach ?>
 
             <tr class="text-right">
-                <td class="text-center" style="border-style: solid; background: navy; color: white;" scope="col"><?php echo $periodikActiveTahun[0]->periode_pertahun; ?></td>
-                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo $periodikActiveTahun[0]->jumlah_donasi; ?></td>
-                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo $periodikActiveTahun[0]->nominal_donasi; ?></td>
-                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo $periodikActiveTahun[0]->jumlah_penerima; ?></td>
-                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo changeNegNumber($periodikActiveTahun[0]->nominal_penyaluran); ?></td>
-                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo changeNegNumber($periodikActiveTahun[0]->lain_lain); ?></td>
+                <td class="text-center" style="border-style: solid; background: navy; color: white;" scope="col"><?php echo $periodikActiveTahun[0]->periode_pertahun ?? '' ?></td>
+                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo $periodikActiveTahun[0]->jumlah_donasi ?? '' ?></td>
+                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo formatAngka($periodikActiveTahun[0]->nominal_donasi ?? '') ?></td>
+                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo $periodikActiveTahun[0]->jumlah_penerima ?? '' ?></td>
+                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo formatAngka($periodikActiveTahun[0]->nominal_penyaluran ?? '' )?></td>
+                <td style="border-style: solid; background: purple; color: white;" scope="col"><?php echo formatAngka($periodikActiveTahun[0]->lain_lain ?? '' )?></td>
                 
-                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo $kumulatifActiveTahun[0]->jumlah_donasi; ?></td>
-                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo $kumulatifActiveTahun[0]->nominal_donasi; ?></td>
-                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo $kumulatifActiveTahun[0]->jumlah_penerima; ?></td>
-                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo changeNegNumber($kumulatifActiveTahun[0]->nominal_penyaluran); ?></td>
-                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo changeNegNumber($kumulatifActiveTahun[0]->lain_lain); ?></td>
+                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo $kumulatifActiveTahun[0]->jumlah_donasi ?? '' ?></td>
+                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo formatAngka($kumulatifActiveTahun[0]->nominal_donasi ?? '') ?></td>
+                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo $kumulatifActiveTahun[0]->jumlah_penerima ?? '' ?></td>
+                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo formatAngka($kumulatifActiveTahun[0]->nominal_penyaluran  ?? '')?></td>
+                <td style="border-style: solid; background: darkslategrey; color: white;" scope="col"><?php echo formatAngka($kumulatifActiveTahun[0]->lain_lain  ?? '')?></td>
 
-                <td style="border-style: solid; background: red; color: white;" scope="col"><?php echo saldoBerjalan($kumulatifActiveTahun[0]); ?></td>
+                <td style="border-style: solid; background: red; color: white;" scope="col"><?php echo formatAngka(saldoBerjalan($kumulatifActiveTahun[0] ?? '' )) ?></td>
                 <td class="text-left" style="border-style: solid;" scope="col">-</td>
             </tr>
 
             <?php $count = 0;
             foreach($periodikPeriode as $perPeriod): ?>
             <tr class="text-right">
-                <td class="text-center" style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo $perPeriod->nama_periode; ?></td>
-                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo $perPeriod->jumlah_donasi; ?></td>
-                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo $perPeriod->nominal_donasi; ?></td>
-                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo $perPeriod->jumlah_penerima; ?></td>
-                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo changeNegNumber($perPeriod->nominal_penyaluran); ?></td>
-                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo changeNegNumber($perPeriod->lain_lain); ?></td>
+                <td class="text-center" style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo $perPeriod->nama_periode ?? '' ?></td>
+                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo $perPeriod->jumlah_donasi ?? '' ?></td>
+                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo formatAngka($perPeriod->nominal_donasi ?? '') ?></td>
+                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo $perPeriod->jumlah_penerima ?? '' ?></td>
+                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo formatAngka($perPeriod->nominal_penyaluran) ?? '' ?></td>
+                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo formatAngka($perPeriod->lain_lain) ?? '' ?></td>
                 
-                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo $kumulatifPeriode[$count]->jumlah_donasi; ?></td>
-                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo $kumulatifPeriode[$count]->nominal_donasi; ?></td>
-                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo $kumulatifPeriode[$count]->jumlah_penerima; ?></td>
-                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo changeNegNumber($kumulatifPeriode[$count]->nominal_penyaluran); ?></td>
-                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo changeNegNumber($kumulatifPeriode[$count]->lain_lain); ?></td>
+                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo $kumulatifPeriode[$count]->jumlah_donasi ?? '' ?></td>
+                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo formatAngka($kumulatifPeriode[$count]->nominal_donasi ?? '') ?></td>
+                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo $kumulatifPeriode[$count]->jumlah_penerima ?? '' ?></td>
+                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo formatAngka($kumulatifPeriode[$count]->nominal_penyaluran  ?? '')?></td>
+                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo formatAngka($kumulatifPeriode[$count]->lain_lain  ?? '')?></td>
 
-                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo saldoBerjalan($kumulatifPeriode[$count]); ?></td>
-                <td class="text-left" style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo $keteranganPeriode[$count]->deskripsi; ?></td>
+                <td style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo formatAngka(saldoBerjalan($kumulatifPeriode[$count]  ?? '' )) ?></td>
+                <td class="text-left" style="border-style: solid; <?php echo $count == $periodikCount ? 'background: yellow;': '' ?>" scope="col"><?php echo $keteranganPeriode[$count]->deskripsi ?? '' ?></td>
             </tr>
             <?php $count++;
             endforeach ?>
